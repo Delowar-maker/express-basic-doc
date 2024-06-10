d3b735d516a99608fd69fef5d00f122ce81fcde3
Md.Delowar Hossain <56905688+Delowar-maker@users.noreply.github.com>
2024-06-10 16:24:47 +0600

    route

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
+app.post('/about', function (req, res) {
+    res.send('about');
+});
+app.put('/contact', function (req, res) {
+    res.send('contact');
+});
+app.delete('/terms', function (req, res) {
+    res.send('terms');
+});
 app.listen(3000);
 console.log('Example app listening on port 3000!');
